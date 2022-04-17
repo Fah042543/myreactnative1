fccfc912ee23a6601f7addeac3315479f02a9e6d
Fah042543 <94782540+Fah042543@users.noreply.github.com>
2022-04-17 17:23:20 +0700

    Week4

--- components/week3/MyIcon.js
+++ components/week3/MyIcon.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View , Text } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 
 
-export default function MyIcon() {
+export default function MyIcon(props) {
     return (
-        <View style={{ justifyContent : 'center' , alignItems : 'center' }}>
-            <FontAwesome name="heart" size={28} color="tomato" />
-            <Text>หัวใจ</Text>            
-        </View>    
+        <View style={{ flex: 1, justifyContent: 'center', alignItems : 'center'}}>
+            <FontAwesome name={ props.name }  size={ props.size } color={ props.color } />
+            <Text>{ props.title }</Text>
+        </View>  
     );
 }
